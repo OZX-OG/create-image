5d0fbfe70d41c81388c9d423ff07e81c51ca77ed
OZX-OG <53302924+OZX-OG@users.noreply.github.com>
2021-12-18 23:56:41 +0100

    Update with_arabic_tran.py

--- with_arabic_tran.py
+++ with_arabic_tran.py
@@ -4,10 +4,10 @@ from deep_translator import GoogleTranslator
 from bidi.algorithm import get_display
 
 def get_quote():
-    response = requests.get("https://zenquotes.io/api/random")
+    response = requests.get("https://zenquotes.io/api/random") #request the quote
     json_data = json.loads(response.text)
     quote = json_data[0]['q'] + " -" + json_data[0]["a"]
-    return (quote)
+    return (quote) 
     
 def draw_multiple_line_text(image, text, font, text_color, text_start_height, arab=False):
     draw = ImageDraw.Draw(image)
@@ -24,30 +24,29 @@ def draw_multiple_line_text(image, text, font, text_color, text_start_height, ar
         y_text += line_height
 
 def make_Image():
-    resolution = (1080, 1920)
+    resolution = (1080, 1920) #resolution of the image
     background_color = (0, 0, 0) #black
     fontsize = 60  
     text_color = (255,255,255)
     text_start_height = 750
 
-    image = Image.new('RGB', resolution, color = background_color)
-    font = ImageFont.truetype("arial.ttf", fontsize)
+    image = Image.new('RGB', resolution, color = background_color) #make new image
+    font = ImageFont.truetype("arial.ttf", fontsize) #set the font
     text_q = "| Quote <==> اقتبس |"
     text_en = get_quote()
-    translated = GoogleTranslator(source='auto', target='ar').translate(text_en)
+
+    translated = GoogleTranslator(source='auto', target='ar').translate(text_en)# translate quote to arabic
     reshaper_text = arabic_reshaper.reshape(translated)
     bidi_text_ar = get_display(reshaper_text)
+
     reshaper_text = arabic_reshaper.reshape(text_q)
     bidi_text_ar_q = get_display(reshaper_text)
 
-    draw_multiple_line_text(image, bidi_text_ar, font, text_color, (text_start_height - 80), True)
-    draw_multiple_line_text(image, bidi_text_ar_q, font, (200,0,0), text_start_height)
-    draw_multiple_line_text(image, text_en, font, text_color, (text_start_height + 80))
-
+    draw_multiple_line_text(image, bidi_text_ar, font, text_color, (text_start_height - 80), True) #arabic
+    draw_multiple_line_text(image, bidi_text_ar_q, font, (200,0,0), text_start_height) #write 'Quote <==> اقتبس'
+    draw_multiple_line_text(image, text_en, font, text_color, (text_start_height + 80)) #write quote
 
     image.save('Image.jpg')
-
-
 
 if __name__ == "__main__":
     make_Image()
